3399bad5b841e373c9d017fa5372eb25e8b7a18f
calieshas <sti15002@byui.edu>
2017-10-09 13:39:53 -0600

    10-07-17 Caliesha's Individual Done 1

---
+++ byuiSeekAndFind/src/byui/cit260/byuiSeekAndFind/model/Item.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.byuiSeekAndFind.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author calie
+ */
+public class Item implements Serializable {
+    
+    private String objectType; 
+    private double numberInPossesion;
+    private String objectsNeeded;
+
+    public Item() {
+    }
+
+    
+    
+    public String getObjectType() {
+        return objectType;
+    }
+
+    public void setObjectType(String objectType) {
+        this.objectType = objectType;
+    }
+
+    public double getNumberInPossesion() {
+        return numberInPossesion;
+    }
+
+    public void setNumberInPossesion(double numberInPossesion) {
+        this.numberInPossesion = numberInPossesion;
+    }
+
+    public String getObjectsNeeded() {
+        return objectsNeeded;
+    }
+
+    public void setObjectsNeeded(String objectsNeeded) {
+        this.objectsNeeded = objectsNeeded;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + java.util.Objects.hashCode(this.objectType);
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.numberInPossesion) ^ (Double.doubleToLongBits(this.numberInPossesion) >>> 32));
+        hash = 29 * hash + java.util.Objects.hashCode(this.objectsNeeded);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Objects{" + "objectType=" + objectType + ", numberInPossesion=" + numberInPossesion + ", objectsNeeded=" + objectsNeeded + '}';
+    }
+
+    
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Item other = (Item) obj;
+        if (Double.doubleToLongBits(this.numberInPossesion) != Double.doubleToLongBits(other.numberInPossesion)) {
+            return false;
+        }
+        if (!java.util.Objects.equals(this.objectType, other.objectType)) {
+            return false;
+        }
+        if (!java.util.Objects.equals(this.objectsNeeded, other.objectsNeeded)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
---
+++ byuiSeekAndFind/src/byui/cit260/byuiSeekAndFind/model/Location.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.byuiSeekAndFind.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author calie
+ */
+public class Location implements Serializable {
+    
+    private String row;
+    private String column;
+    private String visited;
+    private String amountRemaining;
+
+    public Location() {
+    }
+
+    
+    
+    public String getRow() {
+        return row;
+    }
+
+    public void setRow(String row) {
+        this.row = row;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public String getVisited() {
+        return visited;
+    }
+
+    public void setVisited(String visited) {
+        this.visited = visited;
+    }
+
+    public String getAmountRemaining() {
+        return amountRemaining;
+    }
+
+    public void setAmountRemaining(String amountRemaining) {
+        this.amountRemaining = amountRemaining;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.row);
+        hash = 97 * hash + Objects.hashCode(this.column);
+        hash = 97 * hash + Objects.hashCode(this.visited);
+        hash = 97 * hash + Objects.hashCode(this.amountRemaining);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "row=" + row + ", column=" + column + ", visited=" + visited + ", amountRemaining=" + amountRemaining + '}';
+    }
+
+    
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (!Objects.equals(this.row, other.row)) {
+            return false;
+        }
+        if (!Objects.equals(this.column, other.column)) {
+            return false;
+        }
+        if (!Objects.equals(this.visited, other.visited)) {
+            return false;
+        }
+        if (!Objects.equals(this.amountRemaining, other.amountRemaining)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+}
