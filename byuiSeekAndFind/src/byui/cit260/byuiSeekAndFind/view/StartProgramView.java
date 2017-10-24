4f5406e92d3b4a4c92a4c3f7b6e7db73cc136105
breen <breen@10.38.167.160>
2017-10-24 13:42:06 -0600

    10/24/17 team assingment

---
+++ byuiSeekAndFind/src/byui/cit260/byuiSeekAndFind/view/StartProgramView.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.byuiSeekAndFind.view;
+
+/**
+ *
+ * @author breen
+ */
+public class StartProgramView {
+    public void displayStartProgramView(){
+        boolean endView = false;
+        do {
+            string[] inputs = this.getInputs();
+        }
+//      if (no inputs OR first input is Q
+//      RETURN
+//      endView = doAction(inputs)
+//      WHILE endView != true
+//}
+    
+    }
+    
+}
