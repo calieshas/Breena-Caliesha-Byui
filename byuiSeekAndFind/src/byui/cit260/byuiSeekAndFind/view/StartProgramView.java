bc076b642be13e6b673dfe38d05062378180d19f
calieshas <sti15002@byui.edu>
2017-10-25 13:45:22 -0600

    10-25-17 Team Working on-

--- byuiSeekAndFind/src/byui/cit260/byuiSeekAndFind/view/StartProgramView.java
+++ byuiSeekAndFind/src/byui/cit260/byuiSeekAndFind/view/StartProgramView.java
@@ -13,14 +13,36 @@ public class StartProgramView {
     public void displayStartProgramView(){
         boolean endView = false;
         do {
-            string[] inputs = this.getInputs();
-        }
-//      if (no inputs OR first input is Q
-//      RETURN
-//      endView = doAction(inputs)
-//      WHILE endView != true
-//}
-    
+            String[] inputs = this.getInputs();
+            if (inputs[0].toUpperCase().equals("Q") || 
+                    inputs.length < 1 || 
+                    inputs == null) {   
+                return;
+            }
+            
+         endView = doAction(inputs);
+        } while (!endView);
+
     }
-    
+
+
+    private String[] getInputs() {
+        System.out.println("**** getInputs() called ***");
+        
+        String[] inputs = new String[1];
+        inputs[0] = "testInput";
+        
+        return inputs;
+    }
+
+    private boolean doAction(String[] inputs) {
+        System.out.println("**** doAction() called ***");
+        System.out.println("\tinputs = " + inputs[0]);
+        
+        return true;
+    }
+
+    public void display() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
