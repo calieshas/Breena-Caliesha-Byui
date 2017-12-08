28f5e313b10b1f503d77e2bcda258d20575af8a4
breen <breen@10.38.167.160>
2017-12-08 15:03:29 -0700

    12/8/17 with out restart game working

--- byuiSeekAndFind/src/byui/cit260/byuiSeekAndFind/view/SaveGameView.java
+++ byuiSeekAndFind/src/byui/cit260/byuiSeekAndFind/view/SaveGameView.java
@@ -5,9 +5,12 @@
  */
 package byui.cit260.byuiSeekAndFind.view;
 
+import byui.cit260.byuiSeekAndFind.control.GameControl;
 import byui.cit260.byuiSeekAndFind.exception.GameControlException;
 import byui.cit260.byuiSeekAndFind.model.Game;
 import byuiseekandfind.ByuiSeekAndFind;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 
 /**
  *
@@ -26,15 +29,15 @@ public class SaveGameView extends View {
 
     @Override
     public boolean doAction(String[] inputs) {
-//        String filePath = inputs[0]; 
-//        Game game = ByuiSeekAndFind.getCurrentGame();
-//        try{
-//            GameMenuView.saveGame(game);
-//        } catch (GameControlException ex){
-//            ErrorView.display("SaveGameView", ex.getMessage());
-//            return false;
-//        }
-//        this.console.println("Your Game was Saved at the following location" + inputs);
+        String filePath = inputs[0];
+        Game game = ByuiSeekAndFind.getCurrentGame();
+        try {
+            GameControl.saveGame(game, filePath);
+        } catch (GameControlException ex) {
+            ErrorView.display("SaveGameView", ex.getMessage());
+            return false;
+        }
+        this.console.println("Your Game was Saved at the following location" + filePath);
         return true;
     }
 
@@ -49,20 +52,7 @@ public class SaveGameView extends View {
 // Print a success message indicating where the
 // file was saved
 // return true to end the view
-    
-    
     // what is the filePath????????????????????????????????????????????????
-    public static void saveGame(Game game, String filePath) {
-        System.out.println("saveGame() in GameControl class");
-//        if(game || filePath = false){
-        
-}
-//    if invalid a game or filePath passed to method then
-// throw a new GameControlException
-// endIf
-// create a new FileOutputStream for the filePath
-// create a new ObjectOutputStream from the FileOutputStream
-// write the game object to the ObjectOutputStream
     
-    
+
 }
